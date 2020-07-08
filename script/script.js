1cd179e1a4c0a54da7e8f52062e9f75b49c1762e
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-07-08 21:39:30 +0300

    реализована анимация модального окна

--- script/script.js
+++ script/script.js
@@ -51,4 +51,56 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
     countTimer('9 july 2020');
+
+    //Menu
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu  = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    };
+    toggleMenu();
+
+    //popap
+    const togglePopUp = () =>  {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+        let count = 0;
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                const popupModal = function popupModal() {
+                    const width = document.documentElement.clientWidth;
+                    if (width < 768) {
+                        // убрать выполнение анимации
+                        clearTimeout(popupModal, 10);
+                    } else if (count < 200) { //выполнение анимации
+                        count++;
+                        popupContent.style.top = count + 'px';
+                        setTimeout(popupModal, 10);
+                    }
+                };
+                setTimeout(popupModal, 10);
+            });
+        });
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+            count = 0;
+            popupContent.style.top = '10%';
+        });
+    };
+    togglePopUp();
 });
+
