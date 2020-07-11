00bc42b9f18bf7144d86220d3d1b4beeb83902ec
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-07-12 01:27:08 +0300

    регулярка на калькулятор, фото команды

--- script/script.js
+++ script/script.js
@@ -50,7 +50,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 1000);
 
     }
-    countTimer('10 july 2020');
+    countTimer('17 july 2020');
 
     //Menu
     const toggleMenu = () => {
@@ -170,7 +170,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 createDot();
             }
         }
-        console.log(dots);
         const dot = dots.querySelectorAll('.dot');
 
 
@@ -249,5 +248,29 @@ window.addEventListener('DOMContentLoaded', () => {
         startSlide(1500);
     };
     slider();
+    //calc
+    const calc = () => {
+        const calcItem = document.querySelectorAll('.calc-item');
+        calcItem.forEach(item => {
+            item.addEventListener('input', e => {
+                e.target.value = item.value.replace(/\D/g, '');
+            });
+        });
+    };
+    calc();
+    //command
+    const dreamTeam = () => {
+        const commandPhoto = document.querySelectorAll('.command__photo');
+        commandPhoto.forEach(item => {
+            item.addEventListener('mouseenter', e => {
+                item.setAttribute('alt', e.target.src);
+                e.target.src = e.target.dataset.img;
+            });
+            item.addEventListener('mouseleave', e => {
+                e.target.src = e.target.alt;
+            });
+        });
+    };
+    dreamTeam();
 });
 
