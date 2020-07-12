0aa23f9122aeac36a34e31c76bbf7e290ec731d3
Mikhail Gurtsev <gurtsevmr@yandex.ru>
2020-07-12 03:33:15 +0300

    калькуллятор

--- script/script.js
+++ script/script.js
@@ -249,15 +249,53 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     slider();
     //calc
-    const calc = () => {
-        const calcItem = document.querySelectorAll('.calc-item');
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total'),
+            calcItem = document.querySelectorAll('.calc-item');
+        const countSum = () => {
+            let total = 0;
+            let  countValue = 1;
+            let dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+
+        };
+
+        calcBlock.addEventListener('change', e => {
+            const target = e.target;
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+
         calcItem.forEach(item => {
             item.addEventListener('input', e => {
-                e.target.value = item.value.replace(/\D/g, '');
+                e.target.value = item.value.match(/^[0-9]*\.?[0-9]*$/, '');
             });
         });
     };
-    calc();
+    calc(100);
     //command
     const dreamTeam = () => {
         const commandPhoto = document.querySelectorAll('.command__photo');
